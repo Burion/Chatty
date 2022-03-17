5a812b1445ca43e0f989a72bbb8741521ee6adfe
Burion <vladislavburyak00@gmail.com>
2022-03-18 00:19:01 +0200

    [~] main.js to handle SettingsPage

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import LoginPage from "./components/login/LoginPage.vue"
 import ChatPage from "./components/chat/ChatPage.vue"
+import SettingsPage from "./components/settings/SettingsPage.vue"
 import App from "./App.vue"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/login", component: LoginPage },
-    { path: "/chat", component: ChatPage }
+    { path: "/chat", component: ChatPage },
+    { path: "/settings", component: SettingsPage }
   ]
 })
 
