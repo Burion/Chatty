40bf6841e544d5edf02380dccffb024e98ff0557
Burion <vladislavburyak00@gmail.com>
2022-03-17 10:21:16 +0200

    [+] general text styles

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,61 @@
 <template>
-<div class="container">
-  <chat-page></chat-page>
-</div>
+  <div class="container">
+    <chat-page></chat-page>
+  </div>
 </template>
 
 <script>
-
-import ChatPage from './components/chat/ChatPage.vue'
+import ChatPage from "./components/chat/ChatPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ChatPage
-  }
-}
+    ChatPage,
+  },
+};
 </script>
 
 <style>
 .container {
-    margin: auto;
-    width: 1280px;
-    padding: 10px;
-    height: 100%;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  margin: auto;
+  width: 1280px;
+  padding: 10px;
+  height: 100%;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 
 .circle-img-size-1 {
-    background-image: url(https://memepedia.ru/wp-content/uploads/2020/03/gigachad-1-768x432.jpg);
-    height: 40px;
-    width: 40px;
-    background-size: cover;
-    background-position: center;
-    border-radius: 50%;
+  background-image: url(https://memepedia.ru/wp-content/uploads/2020/03/gigachad-1-768x432.jpg);
+  height: 40px;
+  width: 40px;
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
 }
 
 .circle-img-size-2 {
-    background-image: url(https://memepedia.ru/wp-content/uploads/2020/03/gigachad-1-768x432.jpg);
-    height: 50px;
-    width: 50px;
-    background-size: cover;
-    background-position: center;
-    border-radius: 50%;
+  background-image: url(https://memepedia.ru/wp-content/uploads/2020/03/gigachad-1-768x432.jpg);
+  height: 50px;
+  width: 50px;
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+}
+
+.text-regular {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: rgb(70, 70, 70);
+}
+
+.text-small {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: rgb(70, 70, 70);
+  font-size: 8pt;
+}
+
+.text-semi-small {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: rgb(70, 70, 70);
+  font-size: 10pt;
 }
 </style>
