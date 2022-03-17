42fadca42ed528ce335bfbdd5d7f1ac150a0b161
Burion <vladislavburyak00@gmail.com>
2022-03-17 16:50:13 +0200

    [~] Login page

--- src/components/login/LoginPage.vue
+++ src/components/login/LoginPage.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="container">
-    <div class="container-flex-center">
+  <div class="full-height">
+    <the-centered-flex>
       <div class="container-login">
         <label for="">Login</label>
         <input class="input" type="text" />
         <label for="">Password</label>
-        <input class="input" type="text" />
+        <input type="password" class="input" />
         <button class="button">Login</button>
       </div>
-    </div>
+    </the-centered-flex>
   </div>
 </template>
 
 <script>
-export default {};
+import TheCenteredFlex from '../shared/TheCenteredFlex.vue'
+export default {
+  components: { TheCenteredFlex },}
 </script>
 
 <style>
+.full-height {
+  height: 80vh;
+  width: 100%;
+}
+
+.container-flex-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.container-login {
+  display: grid;
+  gap: 5px;
+  width: 400px;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+}
 </style>
