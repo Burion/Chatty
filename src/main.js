105764780f33c87d1205da1d99b086510a05c666
Burion <vladislavburyak00@gmail.com>
2022-03-17 16:09:51 +0200

    [+] router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createRouter, createWebHistory } from "vue-router"
+import LoginPage from "./components/login/LoginPage.vue"
+import ChatPage from "./components/chat/ChatPage.vue"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/login", component: LoginPage },
+    { path: "/chat", component: ChatPage }
+  ]
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount("#app")
