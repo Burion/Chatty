f329c2432b3fae401afca30fd524fc1e3f087358
Burion <vladislavburyak00@gmail.com>
2022-03-17 10:22:18 +0200

    [+] Chat header component

---
+++ src/components/chat/TheChatHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chat-header">
+    <div class="circle-img-size-1"></div>
+    <div class="grid-auto-rows-0">
+      <div class="chat-name">{{ user.name }}</div>
+      <div class="text-semi-small">{{ user.status }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['user']
+};
+</script>
+
+<style>
+</style>
