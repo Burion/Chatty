0af5e605d38a16574fd47bb06e7c01549f821e8f
Burion <vladislavburyak00@gmail.com>
2022-03-17 16:11:49 +0200

    [+] Login page

---
+++ src/components/login/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="container-flex-center">
+      <div class="container-login">
+        <label for="">Login</label>
+        <input class="input" type="text" />
+        <label for="">Password</label>
+        <input class="input" type="text" />
+        <button class="button">Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
