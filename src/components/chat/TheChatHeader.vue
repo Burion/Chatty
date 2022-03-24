c13bbf7f4b2d4a0ed6a89778cec3482c2f645fad
Burion <vladislavburyak00@gmail.com>
2022-03-24 15:56:30 +0200

    [~] TheChatHeader props

--- src/components/chat/TheChatHeader.vue
+++ src/components/chat/TheChatHeader.vue
@@ -2,15 +2,15 @@
   <div class="chat-header">
     <div class="circle-img-size-1"></div>
     <div class="grid-auto-rows-0">
-      <div class="chat-name">{{ user.name }}</div>
-      <div class="text-semi-small">{{ user.status }}</div>
+      <div class="chat-name">{{ chat.name }}</div>
+      <!-- <div class="text-semi-small">{{ user.status }}</div> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['user']
+    props: ['chat']
 };
 </script>
 
