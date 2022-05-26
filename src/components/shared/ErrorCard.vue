b26d7b2d19ed43dd664157bf68ddb61d69d5c4b6
Burion <vladislavburyak00@gmail.com>
2022-05-26 18:06:24 +0300

    [+] error card component

---
+++ src/components/shared/ErrorCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="error-card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .error-card {
+    background-color: rgb(255, 183, 183);
+    border-radius: 10px;
+    color: rgb(59, 59, 59);
+    padding-top: 10px;
+    padding-bottom: 10px;
+    text-align: center;
+    }
+</style>
